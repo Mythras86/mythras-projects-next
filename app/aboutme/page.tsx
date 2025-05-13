584b5959420e8566a7af37fb99367427ccea0059
Mythras <kfarkas86@gmail.com>
2025-05-13 22:07:23 +0200

    about me update

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -17,22 +17,21 @@ export default function Oneletrajz() {
 
       <div className="textCont ">
         <PBlock>
-          I'm a bit of a latecomer to this industry, but I hope, that I can leave my mark and be part of projects that pushes my skills to the edge and beyond! I love planning and problem solving, albeit designing is not my strong suit so to say.
+          I'm a bit of a latecomer to this industry, but I believe, I belong here and I want to be part of projects that pushes my skills to the edge and beyond! I love planning and problem solving and for that I have an urge to learn new things.
         </PBlock>
         <PBlock>
-          Originally I planned to work as an economist or manager as I have a BSC degree in that, but I quickly realized the sour situation: most corporations don't care about this, or employ someone the CEO trust, even if she/he lacks of the skills. Without connections I felt a little disappointed and seeked other chances to employ my abilities.
+          Originally I planned to work as an economist or manager as I have a BSC degree in that, but I quickly realized thats not my way and without good contacts, it's really hard to start a carrier.
         </PBlock>
         <PBlock>
-          From my childhood I enjoyed things that needed some creativity and structural, strategical thinking: chess, lego, later strategic games and RPG's on pc and tabletop. Also tried drawing, messed some time with Photoshop but not that much i could make a living with. These interests made a stable base for my logical thinking, the ability to see the whole picture and to find the connections between... pretty much everything. If I want to highlight a positive attribute, that truly describes me, that is the will to understand.
-        </PBlock>
-        <PBlock>
-          I'm pretty much the lazy type. No, not that one who sleeps all day, but I dont want to do anything unnecessary. For that I always try to fully understand any situation and make a true solution for that. I also understand, that some times you only able to temporary patch things up, but I strongly believe that a full solution is much better.
+          But how I came to web development? Thats a funny story. My elder brother had some lectures in macro and micro economy and asked me to help him understand. We ended up talking about how systems work by the rules and agreed, that logic is a common language between us. Even we didn't know much about each others field of profession, but we still understood the other. I was amazed and thought, that web development is much more creative in its ways I will need less contacts to prove my skills so I wanted to learn it. 
         </PBlock>
         <PBlock>
           To satisfy my curiosity once I filled an international adaptive Mensa test and by the score, my logical IQ can be above 125. The automated email then asked to have a higher test in person to make a more precise measure, but I haven't made it yet. 
         </PBlock>
+        <PBlock>
+          By the way I'm a mostly calm and quiet person and I try to avoid conflicts (but take them, if neccessary). I like talking about meaningful stuff, but I prefer not to chit-chat for no reason. I prefer working in small teams as I think, thats the best way, but I'm not afraid to work alone, or in bigger teams.
+        </PBlock>
       </div>
-
 
     </main>
   );
