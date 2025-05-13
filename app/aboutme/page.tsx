9e02fb9612c55e12ab940d297f4136a5fc627632
Mythras <kfarkas86@gmail.com>
2025-05-13 22:08:12 +0200

    update

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -20,7 +20,7 @@ export default function Oneletrajz() {
           I'm a bit of a latecomer to this industry, but I believe, I belong here and I want to be part of projects that pushes my skills to the edge and beyond! I love planning and problem solving and for that I have an urge to learn new things.
         </PBlock>
         <PBlock>
-          Originally I planned to work as an economist or manager as I have a BSC degree in that, but I quickly realized thats not my way and without good contacts, it's really hard to start a carrier.
+          Originally I wanted to work as an economist or manager as I have a BSC degree in that, but I quickly realized thats not my way and without good contacts, it's really hard to start a carrier.
         </PBlock>
         <PBlock>
           But how I came to web development? Thats a funny story. My elder brother had some lectures in macro and micro economy and asked me to help him understand. We ended up talking about how systems work by the rules and agreed, that logic is a common language between us. Even we didn't know much about each others field of profession, but we still understood the other. I was amazed and thought, that web development is much more creative in its ways I will need less contacts to prove my skills so I wanted to learn it. 
