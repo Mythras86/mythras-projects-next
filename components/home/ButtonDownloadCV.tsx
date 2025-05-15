8e90537b9ef951d1aecec30d7d16e536de68ded8
Mythras <kfarkas86@gmail.com>
2025-05-15 17:26:37 +0200

    target blank

--- components/home/ButtonDownloadCV.tsx
+++ components/home/ButtonDownloadCV.tsx
@@ -5,7 +5,7 @@ export default function ButtonDownloadCV() {
     
 
     return (
-        <Link href={"/files/FKcvENG.pdf"}>
+        <Link href={"/files/FKcvENG.pdf"} target="blank">
             <button className="neonYellow text2 center" type="button">Download CV</button>
         </Link>
     );
