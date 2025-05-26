0dec7ba12c50ea7ce758bac78bc7f22ce3d74f47
Mythras <kfarkas86@gmail.com>
2025-05-26 22:21:20 +0200

    fixes2

--- app/myprojects/shadowrun-in-hungary/components/jellemzok/jellemzok.tsx
+++ app/myprojects/shadowrun-in-hungary/components/jellemzok/jellemzok.tsx
@@ -5,9 +5,9 @@ export default function Jellemzok() {
     return (
       <form id='jellemzok'>
         <h1 className="margBott1">Jellemzők</h1>
-        {jellemzokUtil.map((jellemzo: IJellemzok)=>(
+        {/* {jellemzokUtil.map((jellemzo: IJellemzok)=>(
           <Jellemzo key={jellemzo.key} jellemzo={jellemzo} />
-        ))}
+        ))} */}
       </form>
     );
 }
