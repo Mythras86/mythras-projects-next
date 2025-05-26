a4aa7e768c99c47034e36d0c904da2b4d2d66ef0
Mythras <kfarkas86@gmail.com>
2025-05-26 20:41:58 +0200

    new char form update

--- app/myprojects/shadowrun-in-hungary/ujkarakter/page.tsx
+++ app/myprojects/shadowrun-in-hungary/ujkarakter/page.tsx
@@ -1,17 +1,27 @@
+import Form from "next/form";
 import { jellemzokUtil, IJellemzok } from "../components/jellemzok/data.jellemzok";
 import Jellemzo from "../components/jellemzok/jellemzo/Jellemzo";
+import { useActionState } from "react";
+
+function jellemzokFrissitese(prev: any, formData: FormData) {
+  console.log(formData);
+  return {errors: null }
+}
+
+const [formState, formAction, formPending] = useActionState(jellemzokFrissitese, {errors: null})
 
 export default function Ujkarakter() {
   return (
     <main id='ujkarakter'>
       <h1>Új Karakter</h1>
-      <form id='jellemzok'>
+      <Form action={formAction}>
         <h1 className="margBott1">Jellemzők</h1>
         {jellemzokUtil.filter(x=>x.szerkesztes == false).map((jellemzo: IJellemzok)=>(
           <Jellemzo key={jellemzo.key} jellemzo={jellemzo}>
           </Jellemzo>
         ))}
-      </form>
+        <button type="button" className="text2 neonGreen">Mentés</button>
+      </Form>
 
     </main>
   );
