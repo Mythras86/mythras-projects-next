018087d4b02e00acb85dd2fb90a164cc81a13c84
Mythras <kfarkas86@gmail.com>
2025-05-27 16:38:33 +0200

    03

--- app/myprojects/shadowrun-in-hungary/ujkarakter/page.tsx
+++ app/myprojects/shadowrun-in-hungary/ujkarakter/page.tsx
@@ -1,28 +1,36 @@
-'use client'
+// 'use client'
 
-import Form from "next/form";
-import { useActionState } from "react";
+// import Form from "next/form";
+// import { useActionState } from "react";
 
-function jellemzokFrissitese(prev: any, formData: FormData) {
-  console.log(formData);
-  return {errors: null }
-}
+// function jellemzokFrissitese(prev: any, formData: FormData) {
+//   console.log(formData);
+//   return {errors: null }
+// }
 
-const [formState, formAction, formPending] = useActionState(jellemzokFrissitese, {errors: null})
+// const [formState, formAction, formPending] = useActionState(jellemzokFrissitese, {errors: null})
+
+// export default function Ujkarakter() {
+//   return (
+//     <main id='ujkarakter'>
+//       <h1>Új Karakter</h1>
+//       <Form action={formAction}>
+//         <h1 className="margBott1">Jellemzők</h1>
+//         {/* {jellemzokUtil.filter(x=>x.szerkesztes == false).map((jellemzo: IJellemzok)=>(
+//           <Jellemzo key={jellemzo.key} jellemzo={jellemzo}>
+//           </Jellemzo>
+//         ))} */}
+//         <button type="button" className="text2 neonGreen">Mentés</button>
+//       </Form>
+
+//     </main>
+//   );
+// }
 
 export default function Ujkarakter() {
   return (
     <main id='ujkarakter'>
-      <h1>Új Karakter</h1>
-      <Form action={formAction}>
-        <h1 className="margBott1">Jellemzők</h1>
-        {/* {jellemzokUtil.filter(x=>x.szerkesztes == false).map((jellemzo: IJellemzok)=>(
-          <Jellemzo key={jellemzo.key} jellemzo={jellemzo}>
-          </Jellemzo>
-        ))} */}
-        <button type="button" className="text2 neonGreen">Mentés</button>
-      </Form>
-
+      <div>Yes</div>
     </main>
   );
 }
