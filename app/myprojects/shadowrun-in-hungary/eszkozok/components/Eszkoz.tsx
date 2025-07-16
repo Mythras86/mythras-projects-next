c886e70dd6d00a188a2315d1ddceb2fae24b2619
Mythras <kfarkas86@gmail.com>
2025-07-16 11:58:46 +0200

    import fix

--- app/myprojects/shadowrun-in-hungary/eszkozok/components/Eszkoz.tsx
+++ app/myprojects/shadowrun-in-hungary/eszkozok/components/Eszkoz.tsx
@@ -1,4 +1,4 @@
-import { SzakertelemModel } from "../../karakterek/components/[karakterid]/components/Szakertelmek/store/szakertelem.model";
+import { SzakertelemModel } from "../../karakterek/[karakterid]/components/Szakertelmek/store/szakertelem.model";
 
 interface Props {
     item: SzakertelemModel;
