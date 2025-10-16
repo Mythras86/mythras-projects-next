951bac1901c71a2ef6c2d345d8c87492130e05ac
Mythras86 <kfarkas86@gmail.com>
2025-10-16 22:03:17 +0200

    router function fix

--- app/api/projects/shadowrunInHungary/karakter.route.ts
+++ app/api/projects/shadowrunInHungary/karakter.route.ts
@@ -20,7 +20,7 @@ export async function getTulajdonosNev(email: string) {
   
 }
 
-export async function getOneKarakter(_id: string): Promise<void> {
+export async function getOneKarakter(_id: string): Promise<KarakterDto | undefined> {
 
   await dbConnect();
   
